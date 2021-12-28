0b377a42ed40946170cd56172a8455cedaaa0788
Tiago Sá <satiago@ipvc.pt>
2021-12-28 16:48:13 +0000

    web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/backoffice', function(){
     return view('backoffice/painel');
@@ -61,9 +61,9 @@ Route::get('/locaisAvisitar', function(){
     return view('locaisAvisitar');
 })->name('locaisAvisitar');
 
-Route::get('/ondeComer', function(){
-    return view('ondeComer');
-})->name('ondeComer');
+/*Route::get('/onde-comer', function(){
+    return view('onde-Comer');
+})->name('onde-comer');*/
 
 Route::get('/ondeDormir', function(){
     return view('ondeDormir');
@@ -85,5 +85,28 @@ Route::get('/logout', '\App\Http\Controllers\SessionsController@destroy');
 
 Route::get('/insereGaleria', 'App\Http\Controllers\GalleryController@index')->name('inserirGaleria');
 Route::resource('imagens', 'App\Http\Controllers\GalleryController');
+
+Route::get('/insereOndeComer', 'App\Http\Controllers\OndeComerController@index')->name('ondecomer');
+Route::get('/onde-Comer', 'App\Http\Controllers\OndeComerController@index2')->name('onde-Comer');
+Route::resource('ondeComer', 'App\Http\Controllers\OndeComerController');
+
+
+
+Route::get('/insereHistoria', 'App\Http\Controllers\InsertHistoryController@index')->name('inserirHistoria');
+Route::resource('inserirHistoria', 'App\Http\Controllers\InsertHistoryController');
+Route::put('update-inserirHistoria/{id}', ['as' => 'update-inserirHistoria', 'uses' => 'App\Http\Controllers\InsertHistoryController@update']);
+
+Route::get('/historia', 'App\Http\Controllers\InsertHistoryController@indexPagina')->name('Historia');
+
+Route::get('/insereHeraldica', 'App\Http\Controllers\insereHeraldicaController@index')->name('inserirHeraldica');
+Route::get('/heraldica', 'App\Http\Controllers\insereHeraldicaController@indexSite')->name('Heraldica');
+Route::resource('inserirHeraldica', 'App\Http\Controllers\insereHeraldicaController');
+Route::put('update-insereHeraldica/{id}', ['as' => 'update-insereHeraldica', 'uses' => 'App\Http\Controllers\insereHeraldicaController@update']);
+
+//Caracterizacao 
+Route::get('/insereCaracterizacao', 'App\Http\Controllers\Caracterizacao@index')->name('inserirCaracterizacao');
+Route::resource('inserirCaracterizacao', 'App\Http\Controllers\Caracterizacao');
+Route::put('update-inserirCaracterizacao/{id}', ['as' => 'update-inserirCaracterizacao', 'uses' => 'App\Http\Controllers\Caracterizacao@update']);
+Route::get('/caracterizacao', 'App\Http\Controllers\Caracterizacao@indexSite')->name('caracterizacao');
 
 

56dcbc5ff7da087baacc70981534ddf375505587
Tiago Sá <satiago@ipvc.pt>
2021-12-28 17:13:46 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-<<<<<<< HEAD
-
-
-
-/*Route::get('/registar', function(){
-    return view('registar');
-})->name('registar'); */
-=======
->>>>>>> funcionalidadeDeHistoria
 
 Route::get('/backoffice', function(){
     return view('backoffice/painel');

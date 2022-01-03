232b1ee31c8c5cbfa37fbf062274bc5179c58014
Tiago Sá <satiago@ipvc.pt>
2022-01-03 18:11:31 +0000

    galeriaErroCorrigido

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -39,6 +39,25 @@
     </div>
 </div>
 
+<section class="hero-section">
+  <div class="card-grid">
+      @foreach($categoriasGaleria as $item)
+   
+    <a class="card" href="{{route('open2-Galeria', [$item->id])}}">
+      <div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+      <div class="card__content">
+        <p class="card__category">Categoria</p>
+        <h3 class="card__heading">{{$item->descricao}}</h3>
+      </div>
+    </a>
+  
+    @endforeach
+  <div>
+</section>
+
+
+
+
 
 
 
